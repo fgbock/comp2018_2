f5269800eb4d121ba37a0e96573dc70fffa9f146
fgbock <fernandobock1996@gmail.com>
2018-10-01 09:08:00 -0300

    script de novo

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 rm e3results
 for filename in ./e2tests/*
 do
-    "$filename" | grep -vE '/' >> edited
     (cat "$filename" | ./etapa3) > out 
-    (diff edited out) >> e3results
+    echo "\n" >> e3results
+    (diff "$filename" out) >> e3results
     rm edited
 done
