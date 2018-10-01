50c6f522071a24325fd9a44ef19365af5bec9eae
fgbock <fernandobock1996@gmail.com>
2018-10-01 20:02:04 -0300

    wow!

--- test.sh
+++ test.sh
@@ -5,12 +5,18 @@ do
     touch original
     grep -v "^/" $filename | while read LINE
     do
-        echo $LINE >> original
+        echo $LINE >> original.txt
     done
-    (cat "$filename" | ./etapa3) > new
+    (cat "$filename" |  ./etapa3) > new.txt
+    (cat new.txt | tr -d " \t\n\r") > new
+    (cat original.txt | tr -d " \t\n\r") > original
     echo "\n" >> e3results
     echo "$counter" >> e3results
-    (diff -wbB original new) >> e3results
+    (diff original new) >> e3results
     rm original
+    rm new.txt
+    rm original.txt
+    rm new
     counter=$((counter+1))
+    echo new >> e3detailed
 done
