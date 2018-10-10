6c491960af380aa961db8e48b552ac75f43200e0
fgbock <fernandobock1996@gmail.com>
2018-10-10 11:34:01 -0300

    Melhorado

--- test_e3.py
+++ test_e3.py
@@ -3,7 +3,7 @@ from os.path import isfile, join
 import subprocess
 
 tests_path = "./e3tests/"
-incorrect = [ "asl11", "asl12", "asl14", "asl15", "asl17", "asl21", "asl22", "asl23", "asl26", "asl29", "asl34", "asl40", "asl45", "asl68", "asl69", "asl71", "asl82", "asl83", "asl84", "asl85", "asl86", "asl87", "asl88", "asl89", "asl90", "asl91", "asl92", "asl93", "asl94", "asl95", "asl98", "asl99", "asl100", "asl101", "asl102", "asl103", "asl104", "asl105", "asl106", "asl107", "asl108", "asl109", "asl110", "asl111", "asl112", "asl113", "asl115", "asl116", "asl118", "asl119", "asl120", "asl121", "asl122", "asl123", "asl124", "asl125", "asl127", "asl128", "asl129", "asl130", "asl131", "asl132", "asl133", "asl134", "asl135", "asl136", "asl137"]
+incorrect = [ ]
 onlyfiles = [f for f in listdir(tests_path) if isfile(join(tests_path, f))]
 for f in onlyfiles:
     if f not in incorrect:
@@ -15,6 +15,8 @@ for f in onlyfiles:
 		filedata_aux = message.split()
         if (message and (f not in incorrect) and (msg_aux != filedata_aux)):
             print(f + " incorrect output: - " + message)
-
+	    #print("\n\n")
+	    #print(msg_aux)
+ 	    #print(filedata_aux)
 
 
