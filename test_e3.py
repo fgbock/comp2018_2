2805281b72b4bf6a2ffc88b2aeb632cbe3b0602e
fgbock <fernandobock1996@gmail.com>
2018-10-10 12:31:22 -0300

    should work

--- test_e3.py
+++ test_e3.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import isfile, join
+import os
 import subprocess
+
 
 tests_path = "./e3tests/"
 incorrect = [ ]
@@ -9,14 +11,28 @@ for f in onlyfiles:
     if f not in incorrect:
         p = subprocess.Popen("cat " + tests_path + f + " | ./etapa3", stdout=subprocess.PIPE, shell=True)
         message = p.communicate()[0] 
+	with open('2ndtest', 'w') as file:
+    		file.write(message)
+	r = subprocess.Popen("cat 2ndtest | ./etapa3", stdout=subprocess.PIPE, shell=True)
+	message2 = r.communicate()[0] 
 	msg_aux = message.split()
+	msg_aux2 = message.split()
 	with open(join(tests_path, f), 'r') as myfile:
 		filedata=myfile.read().replace('\n', '')
 		filedata_aux = message.split()
         if (message and (f not in incorrect) and (msg_aux != filedata_aux)):
             print(f + " incorrect output: - " + message)
-	    #print("\n\n")
-	    #print(msg_aux)
- 	    #print(filedata_aux)
+	else : print(f + " had no error on descompila")
+	if (message and (f not in incorrect) and (msg_aux2 != msg_aux)):
+            print(f + " incorrect 2nd descompila: - " + message2)
+	else : print(f + " had no error on 2nd descompila")
+	print("Original: ")
+ 	print(filedata_aux)
+	print("\nDescompila 1: ")
+	print(msg_aux)
+	print("\nDescompila 2: ")
+	print(msg_aux2)
+	print("\n\n")
+	os.remove("2ndtest") 
 
 
