030df91a8da4ccacf3c56037e76ec500b420cf8f
fgbock <fernandobock1996@gmail.com>
2018-10-20 19:40:51 -0300

    oh wow

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ etapa4:
 	gcc -c lex.yy.c
 	gcc -c yyerror.c
 	gcc -c arvore.c
+	gcc -c symboltable.c
 	gcc -c semantic.c
 	gcc -c main.c
 	gcc -o etapa4 main.o arvore.o semantic.o yyerror.o parser.tab.o lex.yy.o
@@ -16,10 +17,11 @@ debug:
 	gcc -g -c lex.yy.c
 	gcc -g -c yyerror.c
 	gcc -g -c arvore.c
+	gcc -c symboltable.c
 	gcc -c semantic.c
 	gcc -g -c main.c
 	gcc -o etapa4 main.o arvore.o semantic.o yyerror.o parser.tab.o lex.yy.o
 
 clean:
-	rm -rf etapa4 lex.yy.c lex.yy.o main.o arvore.o semantic.o yyerror.o parser.tab.c parser.tab.h parser.tab.o parser.output REPORTFILE temp1 temp2
+	rm -rf etapa4 lex.yy.c lex.yy.o main.o arvore.o semantic.o yyerror.o parser.tab.c parser.tab.h parser.tab.o parser.output REPORTFILE temp1 temp2 symboltable.o
 	
--- symboltable.c
+++ symboltable.c
@@ -1,31 +1,72 @@
 #include "symboltable.h"
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-int free_lista(struct *t_lista alvo);
-
-int free_tabela();
-
-int get_entrada(struct *t_lista tabela, struct *t_entrada_simbolo entrada_retorno, *char chave_buscada){
-	struct t_lista entrada = (*tabela);
-	struct t_entrada_simbolo simbolo;
+int free_lista(struct t_lista* lista){
+	struct t_lista* entrada_prev;
+	struct t_lista* entrada = lista;
 	while(entrada != NULL){
-		simbolo = *(entrada->conteudo);
+		entrada_prev = entrada;
+		entrada = entrada->prox;
+		free(entrada_prev);
+	}
+}
+
+int get_entrada(struct t_lista* tabela, struct t_entrada_simbolo* entrada_retorno, char* chave_buscada){
+	struct t_lista* entrada = tabela;
+	struct t_entrada_simbolo* simbolo;
+	while(entrada != NULL){
+		simbolo = entrada->conteudo;
 		if (strcmp(simbolo->chave,chave_buscada)){
-			entrada_retorno = &(simbolo);
+			entrada_retorno = simbolo;
 			return 0;
 		}
 		else{
-			entrada = *(entrada->prox);
+			entrada = entrada->prox;
 		}
 	}
 	return -1;
 }
 
-int set_entrada(struct *t_lista tabela, struct *t_entrada_simbolo entrada_inserida){
-	struct t_lista entrada = (*tabela);
-	while(*(entrada->prox) != NULL){
+int set_entrada(struct t_lista* tabela, struct t_entrada_simbolo* entrada_inserida){
+	struct t_lista* entrada = tabela;
+	struct t_lista* nova_entrada;
+	while(entrada->prox != NULL){
+		entrada = entrada->prox;
+	}
+	nova_entrada->conteudo = entrada_inserida;
+	entrada->prox = nova_entrada;
+	return 0;
+}
+
+// TESTING:
+/*
+int print_lista(struct *t_lista lista){
+	struct *t_lista entrada_prev;
+	struct t_lista entrada = (*lista);
+	while(*entrada != NULL){
+		printf("Chave is %s\n", (*(entrada->conteudo))->chave);
 		entrada = *(entrada->prox);
 	}
-	entrada->prox = &(entrada_inserida);
-};
+}
+
+int main(){
+	int i;
+	char buffer[33];
+	struct t_lista tabela;
+	for (i = 0; i < 10; i++){
+		struct t_entrada_simbolo entr;
+		itoa(i, buffer, 10);
+		entr->chave = strdup(buffer);
+		set_entrada(&tabela, &entr);
+	}
+	print_lista();
+	struct t_entrada_simbolo sought;
+	for (i = 0; i < 10; i++){
+		itoa(i, buffer, 10);
+		get_entrada(&tabela, &sought, buffer);
+		printf();
+	}
+}
+*/
